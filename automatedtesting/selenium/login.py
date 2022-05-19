9b41c1ea51bd1b8becd5742d3aa92ba0075f38d5
lak9483 <104351308+lak9483@users.noreply.github.com>
2022-05-19 17:02:22 +0530

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -9,7 +9,7 @@ def setup():
     options = ChromeOptions()
     options.add_argument("--headless") 
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-extensions")
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options,executable_path='/home/devops/myagent/_work/1/s/automatedtesting/selenium/chromedriver')
     print ('Browser started successfully. Navigating to the demo page to login.')
